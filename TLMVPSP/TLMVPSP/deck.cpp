0486f029a23b6deb393837295bf06f27358d3d51
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2016-12-13 16:00:25 -0500

    Code du mélange de deck
    +correction de coquilles

--- TLMVPSP/TLMVPSP/deck.cpp
+++ TLMVPSP/TLMVPSP/deck.cpp
@@ -14,19 +14,51 @@ deck::~deck()
 // Mélange le deck pour placer chaque question dedans dans un ordre aléatoire
 void deck::melanger()
 {
+	//Les piles de questions temporaires
+	std::stack<question> temp[5];
+
+	//Initialiser le random stuff
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_int_distribution<uint32_t> rn(1, 5);
+	int nombreAleatoire = rn(mt);
+
 	//Un nombre aléatoire de fois
 
-	//Dépiler dans un nombre aléatoire de piles
+	for (auto i = 0; i < nombreAleatoire; ++i)
+	{
+		//Dépiler dans un nombre aléatoire de piles
+		nombreAleatoire = rn(mt);
+		while(_questions.size() !=0)
+			for (auto i = 0; i < nombreAleatoire; ++i)
+				temp[i].push(tirer());
+		
+		//Puis rempiler
+		for (auto i = 0;
+			!(	(temp[0].size() == 0) && 
+				(temp[1].size() == 0) && 
+				(temp[2].size() == 0) && 
+				(temp[3].size() == 0) && 
+				(temp[4].size() == 0)
+			);
+			++i) {
+			if (i > nombreAleatoire) i = 0;
+			if (temp[i].size() > 0)
+			{
+				_questions.push(temp[i].top());
+				temp[i].pop();
+			}
+		}
+	}
 
-	//Puis rempiler
 }
 
 
 // Tire la première question du top du deck
 question deck::tirer()
 {
-	question r = _questions.top;
-	_questions.pop;
+	question r = _questions.top();
+	_questions.pop();
 	return r;
 }
 
