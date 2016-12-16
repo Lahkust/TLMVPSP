cacae9522da040ab08bbbaf51663f722f484f113
brownChristopher <browniechoco36@gmail.com>
2016-12-15 19:33:13 -0500

    main modif

--- TLMVPSP/TLMVPSP/main.cpp
+++ TLMVPSP/TLMVPSP/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, const char **argv)
 	
 	gestionnaire jeu;
 
-	//C0
+	/******C0*******/
 	//button jouer
 	sf::RectangleShape butJouer(sf::Vector2f(150, 100));
 	butJouer.setFillColor(sf::Color(50, 125, 200));
@@ -30,6 +30,11 @@ int main(int argc, const char **argv)
 	sf::RectangleShape butQuitter(sf::Vector2f(150, 100));
 	butQuitter.setFillColor(sf::Color(50, 125, 200));
 	butQuitter.setPosition(sf::Vector2f(325, 475));
+
+
+
+	
+
 
 	//load font
 	sf::Font font;
@@ -54,6 +59,10 @@ int main(int argc, const char **argv)
 	sf::Text txtQuitter("Quitter", font, 50);
 	txtQuitter.setFillColor(sf::Color::Black);
 	txtQuitter.setPosition(sf::Vector2f(325, 475));
+	//texte Le Throne D'En Faire
+	sf::Text txtTitre("Le Throne D'En Faire", font, 70);
+	txtTitre.setFillColor(sf::Color::Black);
+	txtTitre.setPosition(sf::Vector2f(70, 200));
 
 	jeu.init();
 
@@ -62,15 +71,64 @@ int main(int argc, const char **argv)
 	screens[0] = new sf::RenderWindow(mode, "Contrôle");
 	screens[1] = new sf::RenderWindow(mode, "Projecteur");
 
+	sf::Image* normal = new sf::Image;
+	normal->create(10, 10, sf::Color(0, 0, 0, 255));
+	sf::Texture* temp = new sf::Texture;
+	sf::Sprite* temp2 = new sf::Sprite;
+	temp->loadFromImage(*normal);
+	temp2->setTexture(*temp);
+	screens[0]->draw(*temp2);
+
 	while (screens[0]->isOpen()) {						// Is the window still opened?
 		sf::Event event;								// We create an event
+
 		while (screens[0]->pollEvent(event)) {			// We fill the event from the window
-			if (event.type == sf::Event::Closed)		// If the event is close window
-				screens[0]->close();					// We close it ;)
+			cout << sf::Mouse::getPosition().x << " " << sf::Mouse::getPosition().y << endl;
+			cout << butJouer.getPosition().x << " " << butJouer.getPosition().y << endl;
+			switch (event.type)
+			{
+			case sf::Event::Closed :
+				screens[0]->close();
+				break;
+			case sf::Event::MouseButtonPressed :
+				if ((sf::Mouse::getPosition().x > butJouer.getGlobalBounds().left) &&
+					(sf::Mouse::getPosition().x < (butJouer.getGlobalBounds().left + butJouer.getGlobalBounds().width))) 
+					if ((sf::Mouse::getPosition().y > butJouer.getGlobalBounds().top) &&
+						(sf::Mouse::getPosition().y < (butJouer.getGlobalBounds().top + butJouer.getGlobalBounds().height)))
+					{
+						//jeu.play(screens);
+						cout << "allo allo" << endl;
+					}
+				if ((sf::Mouse::getPosition().x > butDemo.getGlobalBounds().left) &&
+					(sf::Mouse::getPosition().x < (butDemo.getGlobalBounds().left + butDemo.getGlobalBounds().width)))
+					if ((sf::Mouse::getPosition().y > butDemo.getGlobalBounds().top) &&
+						(sf::Mouse::getPosition().y < (butDemo.getGlobalBounds().top + butDemo.getGlobalBounds().height)))
+					{
+						//jeu.play(screens);
+						cout << "allo allo" << endl;
+					}
+				if ((sf::Mouse::getPosition().x > butInfo.getGlobalBounds().left) &&
+					(sf::Mouse::getPosition().x < (butInfo.getGlobalBounds().left + butInfo.getGlobalBounds().width)))
+					if ((sf::Mouse::getPosition().y > butInfo.getGlobalBounds().top) &&
+						(sf::Mouse::getPosition().y < (butInfo.getGlobalBounds().top + butInfo.getGlobalBounds().height)))
+					{
+						//jeu.play(screens);
+						cout << "allo allo" << endl;
+					}
+				if ((sf::Mouse::getPosition().x > butQuitter.getGlobalBounds().left) &&
+					(sf::Mouse::getPosition().x < (butQuitter.getGlobalBounds().left + butQuitter.getGlobalBounds().width)))
+					if ((sf::Mouse::getPosition().y > butQuitter.getGlobalBounds().top) &&
+						(sf::Mouse::getPosition().y < (butQuitter.getGlobalBounds().top + butQuitter.getGlobalBounds().height)))
+					{
+						//jeu.play(screens);
+						cout << "allo allo" << endl;
+					}
+					break;
+			}
 		}
 
-		screens[0]->clear();							// This line clear the screen
-		screens[1]->clear();
+		screens[0]->clear(sf::Color::White);							// This line clear the screen
+		screens[1]->clear(sf::Color::White);
 
 		screens[0]->draw(butJouer);
 		screens[0]->draw(butDemo);
@@ -81,10 +139,11 @@ int main(int argc, const char **argv)
 		screens[0]->draw(txtDemo);
 		screens[0]->draw(txtInfo);
 		screens[0]->draw(txtQuitter);
+		screens[1]->draw(txtTitre);
 
 		screens[0]->display();							// And we display the window
 		screens[1]->display();
-		jeu.play(screens);
+		
 	}
 
 	return 0;
